48cae238389128fa1bffbe26e728b62997f20e95
Ombre <ombre@ombre-mbp.local>
2019-05-30 16:31:32 +0200

    practices matching

---
+++ match_practices.R
@@ -0,0 +1,16 @@
+df_practice_dict_legal <- df_practice_dict %>% filter(directory == "Legal 500") %>% select(-directory)
+
+df_data <- dplyr::left_join(x = df_data,
+                            y = df_practice_dict_legal,
+                            by = c("directory_practice" = "practice_original"))
+
+
+
+# save unmatched records --------------------------------------------------
+
+df_practice_unmatched <-
+  df_data %>% filter(is.na(dentons_practice)) %>% select(publisher, directory_practice) %>% group_by(publisher, directory_practice) %>% summarise()
+
+write.csv(df_practice_unmatched,
+          "./data/unmatched/practices_unmatched.csv",
+          row.names = F)
