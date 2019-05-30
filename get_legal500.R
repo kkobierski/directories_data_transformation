39947c6ba2be10bebd9ead9300c606bbaaff918b
Ombre <ombre@ombre-mbp.local>
2019-05-30 13:36:11 +0200

    get legal 500 data from csv

---
+++ get_legal500.R
@@ -0,0 +1,8 @@
+file_name <- "legal500_us_2019_v2"
+file_path <- sprintf("./data/rankings/%s.csv", file_name)
+
+df_legal_raw <- read.csv(file_path, stringsAsFactors = F, sep = ";")
+
+
+colnames(df_legal_raw) <- tolower(colnames(df_legal_raw))
+colnames(df_legal_raw) <- gsub("\\.","_", colnames(df_legal_raw))
