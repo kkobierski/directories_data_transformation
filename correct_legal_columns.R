388a5d49b7731df80761bbd7c12d58ddf0f3d11b
Ombre <ombre@ombre-mbp.local>
2019-05-30 13:36:00 +0200

    unifying Legal 500 columns to desired structure

---
+++ correct_legal_columns.R
@@ -0,0 +1,29 @@
+publisher <- "Legal 500"
+guide <- "Legal 500 US 2019"
+
+
+
+df_legal <-
+  df_legal_raw %>% unite(
+    "directory_practice",
+    c("legal_500_practice_area", "specific_practice_area"),
+    sep = ": "
+  ) %>% mutate(
+    publisher = publisher,
+    guide = guide,
+    directory_region = country,
+    ranking = if_else(
+      table_type == "Recommended firms",
+      as.character(legal_500_tier_ranking),
+      table_type
+    )
+  ) %>% select(
+    year,
+    firm_name,
+    publisher,
+    guide,
+    directory_practice,
+    directory_region,
+    ranking,
+    lawyer_name
+  )
