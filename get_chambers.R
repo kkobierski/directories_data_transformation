20ec79f8941958057b0e6bf3db9b429f1bea9aea
Ombre <ombre@ombre-mbp.local>
2019-12-19 11:47:10 +0100

    minor

--- get_chambers.R
+++ get_chambers.R
@@ -1,5 +1,6 @@
 df_chambers_data <-
   read.csv(
     "~/Documents/projects/Python/get_chambers_data/data/chambers_data.csv",
-    stringsAsFactors = F
+    stringsAsFactors = F,
+    sep = ";"
   )
--- get_legal500.R
+++ get_legal500.R
@@ -1,4 +1,4 @@
-file_name <- "legal_uk_eversheds_final"
+file_name <- "uk_2019_indiv"
 file_path <- sprintf("./data/rankings/%s.csv", file_name)
 
 df_legal_raw <- read.csv(file_path, stringsAsFactors = F, sep = ";")
