82c8a56174a54440f0524df93d2820c8c81d103f
Kobierski <kamil.kobierski@dentons.com>
2020-01-16 09:10:33 +0100

    changed path

--- get_chambers.R
+++ get_chambers.R
@@ -1,6 +1,6 @@
 df_chambers_data <-
   read.csv(
-    "C:/Users/kobiersk/Documents/projects/get_chambers_rankings-master/data/chambers_data.csv",
+    "C:/Users/kobiersk/Documents/projects/get_chambers_rankings/data/chambers_data.csv",
     stringsAsFactors = F,
     sep = ","
   )
