9ced182c4b9a2f8fd82ac6a8b662d5750d6eecd1
Kobierski <kamil.kobierski@dentons.com>
2020-02-19 11:37:30 +0100

    modified separators and encoding for csv data

--- get_chambers.R
+++ get_chambers.R
@@ -1,6 +1,8 @@
 df_chambers_data <-
   read.csv(
-    "C:/Users/kobiersk/Documents/projects/get_chambers_rankings/data/chambers_data.csv",
-    stringsAsFactors = F,
+    "C:/Users/kobiersk/Documents/projects/get_chambers_rankings_ipython/data/chambers_data.csv",
+    stringsAsFactors = F, 
+    fileEncoding = "utf-8",
+    encoding = "utf-8",
     sep = ","
   )
--- get_legal500.R
+++ get_legal500.R
@@ -1,8 +1,9 @@
-file_name <- "uk_2019_indiv"
-file_path <- sprintf("./data/rankings/%s.csv", file_name)
+file_name <- "legal500_AP2020"
+file_path <- sprintf("C:/Users/kobiersk/Documents/Directories/Legal 500/%s.csv", file_name)
 
-df_legal_raw <- read.csv(file_path, stringsAsFactors = F, sep = ";")
+df_legal_raw <- read.csv(file_path, stringsAsFactors = F, sep = ",")
 
 
 colnames(df_legal_raw) <- tolower(colnames(df_legal_raw))
 colnames(df_legal_raw) <- gsub("\\.","_", colnames(df_legal_raw))
+colnames(df_legal_raw) <- gsub(".*country", "country", colnames(df_legal_raw))
